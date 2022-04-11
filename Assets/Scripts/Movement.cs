d2f72916e26235ec1210e1584977e23fdddd392b
Tangel2002 <tangel@ltu.edu>
2022-04-11 11:50:13 -0400

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject fp;
+    public float mouseSens = 50;
+    public float speed = 15;
+    float xRotation = 0;
+    public CharacterController characterController;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float mouseXAxis = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseYAxis = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseYAxis;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        fp.gameObject.transform.Rotate(Vector3.up * mouseXAxis);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float verticle = Input.GetAxis("Vertical");
+
+        Vector3 move = fp.transform.right * horizontal + transform.forward * verticle;
+
+        characterController.Move(move * speed * Time.deltaTime);
+
+    }
+
+
+        
+}
